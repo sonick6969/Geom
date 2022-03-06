911a4f170f65ced73a0349689213215d98fb16f0
alex <aok@buran.rest>
2022-03-06 18:01:56 +0300

    colors works

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,8 +1,13 @@
 package app;
 
 import io.github.humbleui.jwm.*;
+import io.github.humbleui.jwm.skija.EventFrameSkija;
+import io.github.humbleui.skija.Surface;
 
+import java.io.File;
 import java.util.function.Consumer;
+
+import static app.Colors.APP_BACKGROUND_COLOR;
 
 /**
  * Класс окна приложения
@@ -21,6 +26,40 @@ public class Application implements Consumer<Event> {
         window = App.makeWindow();
         // задаём обработчиком событий текущий объект
         window.setEventListener(this);
+        // задаём заголовок
+        window.setTitle("Java 2D");
+        // задаём размер окна
+        window.setWindowSize(900, 900);
+        // задаём его положение
+        window.setWindowPosition(100, 100);
+        // задаём иконку
+
+        switch (Platform.CURRENT) {
+            case WINDOWS -> window.setIcon(new File("src/main/resources/windows.ico"));
+            case MACOS -> window.setIcon(new File("src/main/resources/macos.icns"));
+        }
+
+        // названия слоёв, которые будем перебирать
+        String[] layerNames = new String[]{
+                "LayerGLSkija", "LayerRasterSkija"
+        };
+
+        // перебираем слои
+        for (String layerName : layerNames) {
+            String className = "io.github.humbleui.jwm.skija." + layerName;
+            try {
+                Layer layer = (Layer) Class.forName(className).getDeclaredConstructor().newInstance();
+                window.setLayer(layer);
+                break;
+            } catch (Exception e) {
+              System.out.println("Ошибка создания слоя " + className);
+            }
+        }
+
+        // если окну не присвоен ни один из слоёв
+        if (window._layer == null)
+            throw new RuntimeException("Нет доступных слоёв для создания");
+
         // делаем окно видимым
         window.setVisible(true);
     }
@@ -38,6 +77,9 @@ public class Application implements Consumer<Event> {
             App.terminate();
         } else if (e instanceof EventWindowCloseRequest) {
             window.close();
+        }else if (e instanceof EventFrameSkija ee) {
+            Surface s = ee.getSurface();
+            s.getCanvas().clear(APP_BACKGROUND_COLOR);
         }
     }
 }
