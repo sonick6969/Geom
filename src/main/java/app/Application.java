9c76ea7a87c4cf183968b7170f802f7a17c795b5
fadeevasa.24 <sofifadeeva@yandex.ru>
2023-02-18 09:54:13 +0300

    4 panels

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -9,6 +9,10 @@ import io.github.humbleui.skija.RRect;
 import io.github.humbleui.skija.Surface;
 import misc.CoordinateSystem2i;
 import misc.Misc;
+import panels.PanelControl;
+import panels.PanelHelp;
+import panels.PanelLog;
+import panels.PanelRendering;
 
 import java.io.File;
 import java.util.function.Consumer;
@@ -27,7 +31,7 @@ public class Application implements Consumer<Event> {
     /**
      * отступы панелей
      */
-    private static final int PANEL_PADDING = 5;
+    public static final int PANEL_PADDING = 5;
     /**
      * радиус скругления элементов
      */
@@ -36,16 +40,23 @@ public class Application implements Consumer<Event> {
     /**
      * Первый заголовок
      */
-    private final Label label;
 
     /**
-     * Первый заголовок
+     * панель легенды
      */
-    private final Label label2;
+    private final PanelHelp panelHelp;
     /**
-     * Первый заголовок
+     * панель курсора мыши
      */
-    private final Label label3;
+    private final PanelControl panelControl;
+    /**
+     * панель рисования
+     */
+    private final PanelRendering panelRendering;
+    /**
+     * панель событий
+     */
+    private final PanelLog panelLog;
 
 
 
@@ -55,6 +66,29 @@ public class Application implements Consumer<Event> {
     public Application() {
         // создаём окно
         window = App.makeWindow();
+
+        // создаём панель рисования
+        panelRendering = new PanelRendering(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 0, 0,
+                3, 2
+        );
+        // создаём панель управления
+        panelControl = new PanelControl(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 3, 0,
+                2, 2
+        );
+        // создаём панель лога
+        panelLog = new PanelLog(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 0, 2,
+                3, 1
+        );
+        // создаём панель помощи
+        panelHelp = new PanelHelp(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 3, 2,
+                2, 1
+        );
+
+
         // задаём обработчиком событий текущий объект
 
         window.setEventListener(this);
@@ -64,15 +98,9 @@ public class Application implements Consumer<Event> {
         window.setWindowSize(900, 900);
         // задаём его положение
         window.setWindowPosition(100, 100);
-        label = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
-                4, 4, 1, 1, 1, 1, "Привет, мир!", true, true);
-        // создаём второй заголовок
-        label2 = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
-                4, 4, 0, 3, 1, 1, "Второй заголовок", true, true);
+        // создаём панель рисования
 
-        // создаём третий заголовок
-        label3 = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
-                4, 4, 2, 0, 1, 1, "Это тоже заголовок", true, true);
+
 
         switch (Platform.CURRENT) {
             case WINDOWS -> window.setIcon(new File("src/main/resources/windows.ico"));
@@ -129,28 +157,16 @@ public class Application implements Consumer<Event> {
      * @param canvas   низкоуровневый инструмент рисования примитивов от Skija
      * @param windowCS СК окна
      */
-    /**
-     * Рисование
-     *
-     * @param canvas   низкоуровневый инструмент рисования примитивов от Skija
-     * @param windowCS СК окна
-     */
     public void paint(Canvas canvas, CoordinateSystem2i windowCS) {
         // запоминаем изменения (пока что там просто заливка цветом)
         canvas.save();
         // очищаем канвас
         canvas.clear(APP_BACKGROUND_COLOR);
-
-        // рисуем заголовок в точке [100,100] с шириной и выостой 200
-        label.paint(canvas, windowCS);
-
-        // рисуем второй заголовок
-        label2.paint(canvas, windowCS);
-        // рисуем третий заголовок
-        label3.paint(canvas, windowCS);
-
-        // восстанавливаем состояние канваса
+        // рисуем панели
+        panelRendering.paint(canvas, windowCS);
+        panelControl.paint(canvas, windowCS);
+        panelLog.paint(canvas, windowCS);
+        panelHelp.paint(canvas, windowCS);
         canvas.restore();
-
     }
 }
